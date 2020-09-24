813019fd3d75bdec47ca632144800d0469a03749
Milka <43339411+Camille0907@users.noreply.github.com>
2020-09-24 15:43:31 +0200

    The checkBoxes work

--- lib/Resources/appStrings.dart
+++ lib/Resources/appStrings.dart
@@ -10,15 +10,47 @@ class AppStrings {
       ['Je peux lire des textes courts très simples.',
        'Je peux comprendre la description d\'événements, l\'expression de sentiments et de souhaits dans des lettres personnelles.'],
     'Compréhension orale': [
-        'Je peux comprendre des mots familiers et des expressions très courantes au sujet de moi-même, de ma famille et del\'environnement concret et immédiat, si les gens parlent lentement et distinctement.',
+        'Je peux comprendre des mots familiers et des expressions très courantes au sujet de moi-même, de ma famille et de l\'environnement concret et immédiat, si les gens parlent lentement et distinctement.',
         'Je peux comprendre les émissions de télévision et les films sans trop d\'effort.'],
     'Expression écrite': [
         'Je peux écrire des textes clairs etdétaillés sur une grande gamme desujets relatifs à mes intérêts.',
-        'Je peux comprendre les émissions de télévision et les films sans trop d\'effort.'],
+        'Je peux comprendre les livres sans trop d\'effort.'],
     'Expression orale': [
         'Je peux utiliser une série de phrases ou d\'expressions pour décrire en termes simples ma famille et d\'autres gens, mes conditions de vie, ma formation et mon activité professionnelle actuelle ou récente.',
         'Je peux raconter une histoire ou l\'intrigue d\'un livre ou d\'un film et exprimer mes réactions.'
     ]};
+  static const LEVELS_BY_SKILL = {
+    'Je peux lire des textes courts très simples.' : 'A2',
+    'Je peux comprendre la description d\'événements, l\'expression de sentiments et de souhaits dans des lettres personnelles.' : 'B1',
+    'Je peux comprendre des mots familiers et des expressions très courantes au sujet de moi-même, de ma famille et de l\'environnement concret et immédiat, si les gens parlent lentement et distinctement.' : 'A1',
+    'Je peux comprendre les émissions de télévision et les films sans trop d\'effort.' : 'C1',
+    'Je peux écrire des textes clairs etdétaillés sur une grande gamme desujets relatifs à mes intérêts.' : 'B2',
+    'Je peux comprendre les livres sans trop d\'effort.' : 'C2',
+    'Je peux utiliser une série de phrases ou d\'expressions pour décrire en termes simples ma famille et d\'autres gens, mes conditions de vie, ma formation et mon activité professionnelle actuelle ou récente.' : 'A2',
+    'Je peux raconter une histoire ou l\'intrigue d\'un livre ou d\'un film et exprimer mes réactions.': 'B1'
+  };
+
+  static const TEACHERS_CHECKS_BY_SKILL = {
+    'Je peux lire des textes courts très simples.' : true,
+    'Je peux comprendre la description d\'événements, l\'expression de sentiments et de souhaits dans des lettres personnelles.' : false,
+    'Je peux comprendre des mots familiers et des expressions très courantes au sujet de moi-même, de ma famille et de l\'environnement concret et immédiat, si les gens parlent lentement et distinctement.' : true,
+    'Je peux comprendre les émissions de télévision et les films sans trop d\'effort.' : false,
+    'Je peux écrire des textes clairs etdétaillés sur une grande gamme desujets relatifs à mes intérêts.' : false,
+    'Je peux comprendre les livres sans trop d\'effort.' : false,
+    'Je peux utiliser une série de phrases ou d\'expressions pour décrire en termes simples ma famille et d\'autres gens, mes conditions de vie, ma formation et mon activité professionnelle actuelle ou récente.' : true,
+    'Je peux raconter une histoire ou l\'intrigue d\'un livre ou d\'un film et exprimer mes réactions.': true
+  };
+
+  static const AUTO_CHECKS_BY_SKILL = {
+    'Je peux lire des textes courts très simples.' : true,
+    'Je peux comprendre la description d\'événements, l\'expression de sentiments et de souhaits dans des lettres personnelles.' : false,
+    'Je peux comprendre des mots familiers et des expressions très courantes au sujet de moi-même, de ma famille et de l\'environnement concret et immédiat, si les gens parlent lentement et distinctement.' : true,
+    'Je peux comprendre les émissions de télévision et les films sans trop d\'effort.' : false,
+    'Je peux écrire des textes clairs etdétaillés sur une grande gamme desujets relatifs à mes intérêts.' : false,
+    'Je peux comprendre les livres sans trop d\'effort.' : false,
+    'Je peux utiliser une série de phrases ou d\'expressions pour décrire en termes simples ma famille et d\'autres gens, mes conditions de vie, ma formation et mon activité professionnelle actuelle ou récente.' : true,
+    'Je peux raconter une histoire ou l\'intrigue d\'un livre ou d\'un film et exprimer mes réactions.': true
+  };
 
 static const SKILLS = {   'Compréhension écrite': [
       {'A2', 'Je peux lire des textes courts très simples.'},
@@ -30,7 +62,7 @@ static const SKILLS = {   'Compréhension écrite': [
     'Compréhension orale': [
       {
         'A1',
-        'Je peux comprendre des mots familiers et des expressions très courantes au sujet de moi-même, de ma famille et del\'environnement concret et immédiat, si les gens parlent lentement et distinctement.'
+        'Je peux comprendre des mots familiers et des expressions très courantes au sujet de moi-même, de ma famille et de l\'environnement concret et immédiat, si les gens parlent lentement et distinctement.'
       },
       {
         'C1',
@@ -44,7 +76,7 @@ static const SKILLS = {   'Compréhension écrite': [
       },
       {
         'C2',
-        'Je peux comprendre les émissions de télévision et les films sans trop d\'effort.'
+        'Je peux comprendre les livres sans trop d\'effort.'
       }
     ],
     'Expression orale': [
