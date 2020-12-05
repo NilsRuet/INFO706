eb889614b909ff3c0d2159398799f3291c1741a0
Zdepu <xavnour@yahoo.com>
2020-12-05 19:49:31 +0100

    Connexion persistante propre

--- FlutterApplication/lib/main.dart
+++ FlutterApplication/lib/main.dart
@@ -5,7 +5,6 @@ import 'package:info706/View/Common/common_drawer.dart';
 import 'package:info706/View/Student/Pages/skills_route.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:info706/Model/Authentication/authentication_service.dart';
 import 'package:provider/provider.dart';
 import 'package:info706/View/sign_in_page.dart';
 
