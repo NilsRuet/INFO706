ae7b8b3f09fa90153b95f7954bcf4a31044b1169
Zdepu <xavnour@yahoo.com>
2020-12-05 12:36:03 +0100

    Implementation de l'authentification via Google

---
+++ lib/View/sign_in_page.dart
@@ -0,0 +1,75 @@
+import 'package:info706/Model/Authentication/authentication_service.dart';
+import 'package:flutter/material.dart';
+import 'package:info706/Model/Authentication/sign_in.dart';
+import 'package:info706/View/home_route.dart';
+import 'package:provider/provider.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.black,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlutterLogo(size: 150),
+              SizedBox(height: 50),
+              _signInButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _signInButton() {
+    return OutlineButton(
+      splashColor: Colors.grey,
+      onPressed: authenticateCallback,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      highlightElevation: 0,
+      borderSide: BorderSide(color: Colors.grey),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Google',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  authenticateCallback() {
+    SignIn.signInWithGoogle().then((result) {
+      if (result != null) {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) {
+              return HomeView();
+            },
+          ),
+        );
+      }
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -5,44 +5,21 @@ import 'package:info706/View/Common/common_drawer.dart';
 import 'package:info706/View/Student/Pages/skills_route.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:info706/authentication_service.dart';
+import 'package:info706/Model/Authentication/authentication_service.dart';
 import 'package:provider/provider.dart';
-import 'package:info706/sign_in_page.dart';
+import 'package:info706/View/sign_in_page.dart';
 
-Future<void> main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<AuthenticationService>(
-          create: (_) => AuthenticationService(FirebaseAuth.instance),
-        ),
-        StreamProvider(
-            create: (context) => context.read<AuthenticationService>().authStateChanges,
-        )
-      ],
-      child: MaterialApp(
-        theme: ThemeData.dark(),
-        home: AuthenticationWrapper(),
-      ),
+    return MaterialApp(
+      title: 'Flutter Login',
+      theme: ThemeData.dark(),
+      home: LoginPage(),
     );
-  }
-}
-
-class AuthenticationWrapper extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final firebaseUser = context.watch<User>();
-
-    if (firebaseUser != null) {
-      return HomeView();
-    }
-    return SignInPage();
   }
 }
