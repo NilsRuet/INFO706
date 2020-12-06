41e2c9e0bdb8618746edfdccf2720008442c0f11
Nils <nils.ruet@etu.univ-smb.fr>
2020-12-06 15:36:22 +0100

    Toutes les images sont désomrais dans le cache une fois téléchargées

--- FlutterApplication/lib/View/Common/common_drawer.dart
+++ FlutterApplication/lib/View/Common/common_drawer.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:info706/Model/Data/User.dart';
 import 'package:info706/View/home_route.dart';
@@ -16,7 +17,7 @@ class _DrawerState extends State<NavigationDrawer>{
               child: Row(
                 children: [
                   CircleAvatar(
-                    backgroundImage: NetworkImage(widget.currentUser.picURL),
+                    backgroundImage: CachedNetworkImageProvider(widget.currentUser.picURL),
                     radius: 40.0,
                   ),
                   Expanded(
